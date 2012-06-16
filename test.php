88d4a49d15f9fcfe8b67f0148b0a05b0d103c7bb
kesspess <kesspess@gmail.com>
2012-06-16 13:39:11 +0200

    basic test added

---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once __DIR__ . '/interface/IAutomaton.php';
+require_once __DIR__ . '/interface/IAutomatonFactory.php';
+require_once __DIR__ . '/interface/IState.php';
+
+require_once __DIR__ . '/Automaton.php';
+require_once __DIR__ . '/State.php';
+
+echo "Hello world.\n";

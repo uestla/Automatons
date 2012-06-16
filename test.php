5357b83ca1b225c6f2aaf654c501a3cbc300e69f
kesspess <kesspess@gmail.com>
2012-06-16 13:49:07 +0200

    Nette Debugger temporarily added

--- test.php
+++ test.php
@@ -1,10 +1,14 @@
 <?php
 
+require_once __DIR__ . '/vendors/Nette/nette.min.php';
+
 require_once __DIR__ . '/interface/IAutomaton.php';
 require_once __DIR__ . '/interface/IAutomatonFactory.php';
 require_once __DIR__ . '/interface/IState.php';
 
 require_once __DIR__ . '/Automaton.php';
 require_once __DIR__ . '/State.php';
+
+Nette\Diagnostics\Debugger::enable();
 
 echo "Hello world.\n";

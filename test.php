42935e4295d7f6ddadc4d054b7c5aefa38810389
kesspess <kesspess@gmail.com>
2012-06-17 10:54:30 +0200

    automaton example file renamed

--- test.php
+++ test.php
@@ -4,6 +4,6 @@ require_once __DIR__ . '/src/Automaton.php';
 require_once __DIR__ . '/src/factories/IFactory.php';
 require_once __DIR__ . '/src/factories/FileFactory.php';
 
-$factory = new Automaton\FileFactory(__DIR__ . '/example.txt');
+$factory = new Automaton\FileFactory(__DIR__ . '/automaton.txt');
 $automaton = $factory->create();
 dump($automaton);

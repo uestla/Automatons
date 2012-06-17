7ddff3e5dba92fa888e057eab92ff38559bec83f
kesspess <kesspess@gmail.com>
2012-06-17 10:36:55 +0200

    vendors removed

--- test.php
+++ test.php
@@ -1,36 +1,3 @@
 <?php
 
-require_once __DIR__ . '/vendors/Nette/nette.min.php';
 require_once __DIR__ . '/src/Automaton.php';
-
-Nette\Diagnostics\Debugger::enable( Nette\Diagnostics\Debugger::DEVELOPMENT );
-Nette\Diagnostics\Debugger::$maxDepth = FALSE;
-Nette\Diagnostics\Debugger::$strictMode = TRUE;
-
-$states = array(
-	'A' => array(
-		'a' => array('B'),
-		'b' => array(),
-		'c' => array('C', 'A'),
-		'' => array(),
-	),
-	'B' => array(
-		'a' => array('A'),
-		'b' => array('A'),
-		'c' => array('A', 'B'),
-		'' => array('A'),
-	),
-	'C' => array(
-		'a' => array('C'),
-		'b' => array('C'),
-		'c' => array('C'),
-		'' => array(),
-	),
-);
-
-$initials = array('A');
-$finals = array('A', 'C');
-
-$automaton = new Automaton($states, $initials, $finals);
-$automaton->determinize()->normalize();
-dump($automaton);

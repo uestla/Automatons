07dc7e2a912e0abe97ba8e2e60197e1181b863a7
Petr Kessler <kesspess@gmail.com>
2013-02-04 16:59:58 +0100

    typos

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,7 +6,7 @@ require_once 'Nette/loader.php';
 require_once 'PHPUnit/Autoload.php';
 
 
-Debugger::enable(FALSE, FALSE);
+Debugger::enable(Debugger::DEVELOPMENT, FALSE);
 Debugger::$strictMode = TRUE;
 Debugger::$maxDepth = FALSE;
 Debugger::$maxLen = FALSE;
